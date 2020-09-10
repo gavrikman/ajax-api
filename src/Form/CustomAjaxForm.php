8d651446cdefb53671cebfdb52617711c51a5653
AndrewG <andrew.g@alphawebgroup.com>
2020-09-10 17:45:52 +0300

    Fixed code for Drupal coding standart

--- src/Form/CustomAjaxForm.php
+++ src/Form/CustomAjaxForm.php
@@ -13,63 +13,13 @@ use Drupal\Core\Form\FormStateInterface;
 class CustomAjaxForm extends FormBase{
 
 
-  public function getFormId()
-  {
+  public function getFormId() {
     return 'custom_ajax_form';
   }
 
-  /*public function buildForm(array $form, FormStateInterface $form_state) {
-
-
-
-
-
-
-      $form['field_wrapper'] = [
-        '#type' => 'container',
-        '#tree' => TRUE,
-        '#prefix' => '<div>',
-        '#suffix' => '</div>',
-      ];
-
-
-      return $form;
-    }
-
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-
-    }
-
-    public function addFieldsCallback(&$form, FormStateInterface $form_state) {
-      $current_storage = $form_state->getStorage();
-      if (isset($current_storage['field_wrapper'])) {
-        $form_state->setStorage($current_storage['field_wrapper']);
-      } else {
-        $form_state->setStorage(['field_wrapper' => []]);
-      }
-
-      $form_state->setStorage((array)$current_storage['field_wrapper']);
-      $current_storage = $form_state->getStorage();
-      if ($current_storage['field_wrapper']) {
-        for ($i = 1; $i <= $current_storage['field_wrapper']; $i++) {
-          $form['field_wrapper'][$i] = [
-            '#type' => 'textfield',
-            '#placeholder' => 'New text',
-            '#tree' => true,
-          ];
-        }
-      }
-
-      $current_storage['field_wrapper']++;
-      $form_state->setRebuild();
-      return $form['field_wrapper'];
-
-    }
-  }*/
   protected $number;
 
-  public function buildForm(array $form, FormStateInterface $form_state)
-  {
+  public function buildForm(array $form, FormStateInterface $form_state) {
 
     $form['#tree'] = TRUE;
 
